3e3a0f1e3dcc917988a13650f2ed8b916edf521b
Hritick Jaiswal <hritickjaiswal22@gmail.com>
2023-08-30 11:54:02 +0530

    #1.3 curry fn

--- curry/index.js
+++ curry/index.js
@@ -14,18 +14,47 @@
 // console.log(curried1(8));
 // console.log(curried1(0));
 
-function curry2(...initialArgs) {
-  let result = initialArgs;
+// function curry2(...initialArgs) {
+//   let result = initialArgs;
+
+//   function inner(...args) {
+//     if (args.length === 0) return result.reduce((acc, val) => acc + val, 0);
+
+//     result = [...result, ...args];
+//     return inner;
+//   }
+
+//   return inner;
+// }
+
+// const total = curry2(1, 1, 1, 1)(2)(3, 3)(4, 4)();
+// console.log(total);
+
+function sum(a, b, c, d) {
+  return a + b + c + d;
+}
+
+function curry(fn) {
+  const argsLength = fn.length;
+  let result = [];
 
   function inner(...args) {
-    if (args.length === 0) return result.reduce((acc, val) => acc + val, 0);
+    result.push(...args);
 
-    result = [...result, ...args];
+    if (result.length >= argsLength) {
+      const temp = result.slice(0, argsLength);
+      result = [];
+      return fn(...temp);
+    }
+
     return inner;
   }
 
   return inner;
 }
 
-const total = curry2(1, 1, 1, 1)(2)(3, 3)(4, 4)();
-console.log(total);
+const curried3 = curry(sum);
+
+console.log(curried3(1, 2, 3, 4));
+console.log(curried3()(2, 3)(4)(1));
+console.log(curried3(1)(2)(3)(4));
