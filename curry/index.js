f4fc417897ac936039975395a9a24bf5de8b6dad
Hritick Jaiswal <hritickjaiswal22@gmail.com>
2023-08-30 11:25:54 +0530

    #1.1 curry 2

--- curry/index.js
+++ curry/index.js
@@ -1,15 +1,31 @@
-function curry1() {
-  let result = 0;
+// function curry1() {
+//   let result = 0;
 
-  return function (val = 0) {
+//   return function (val = 0) {
+//     result += val;
+//     return result;
+//   };
+// }
+
+// const curried1 = curry1();
+// console.log(curried1(5));
+// console.log(curried1(5));
+// console.log(curried1(3));
+// console.log(curried1(8));
+// console.log(curried1(0));
+
+function curry2(initialValue = 0) {
+  let result = initialValue;
+
+  function inner(val) {
+    if (val === undefined) return result;
+
     result += val;
-    return result;
-  };
+    return inner;
+  }
+
+  return inner;
 }
 
-const curried1 = curry1();
-console.log(curried1(5));
-console.log(curried1(5));
-console.log(curried1(3));
-console.log(curried1(8));
-console.log(curried1(0));
+const total = curry2(10)(20)(30)(40)();
+console.log(total);
