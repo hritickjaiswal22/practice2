9bafece7b730db32c2488cae743e31dd00abecbb
Hritick Jaiswal <hritickjaiswal22@gmail.com>
2023-09-08 11:31:52 +0530

    #1.1 pipe functions

--- pipe/index.js
+++ pipe/index.js
@@ -1,34 +1,46 @@
-function pipe(parentObj) {
-  function find(obj, argsArr) {
-    const result = {};
+// function pipe(parentObj) {
+//   function find(obj, argsArr) {
+//     const result = {};
 
-    for (const key of Object.keys(obj)) {
-      const val = obj[key];
+//     for (const key of Object.keys(obj)) {
+//       const val = obj[key];
 
-      if (typeof val === "function") {
-        result[key] = val(...argsArr);
-      } else if (typeof val === "object" && !Array.isArray(val)) {
-        const returnedObj = find(val, argsArr);
+//       if (typeof val === "function") {
+//         result[key] = val(...argsArr);
+//       } else if (typeof val === "object" && !Array.isArray(val)) {
+//         const returnedObj = find(val, argsArr);
 
-        if (returnedObj) result[key] = returnedObj;
-      } else result[key] = val;
+//         if (returnedObj) result[key] = returnedObj;
+//       } else result[key] = val;
+//     }
+
+//     if (Object.keys(result).length === 0) return null;
+//     return result;
+//   }
+
+//   return function (...args) {
+//     return find(parentObj, args);
+//   };
+// }
+
+// console.log(
+//   pipe({
+//     a: {
+//       b: (x, y, z) => x + y + z,
+//       c: (x, y, z) => x + y - z,
+//     },
+//     d: (x, y, z) => x - y - z,
+//   })(1, 1, 1)
+// );
+
+function pipe(...fns) {
+  return function (val) {
+    let temp = val;
+
+    for (const fn of fns) {
+      temp = fn(temp);
     }
 
-    if (Object.keys(result).length === 0) return null;
-    return result;
-  }
-
-  return function (...args) {
-    return find(parentObj, args);
+    return temp;
   };
 }
-
-console.log(
-  pipe({
-    a: {
-      b: (x, y, z) => x + y + z,
-      c: (x, y, z) => x + y - z,
-    },
-    d: (x, y, z) => x - y - z,
-  })(1, 1, 1)
-);
