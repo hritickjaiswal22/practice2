9518533df8af6ac0b891c26549d79356e332cf6b
Hritick Jaiswal <hritickjaiswal22@gmail.com>
2023-10-25 17:28:22 +0530

    1.1 promisePolyfill

--- promisePolyfill/index.js
+++ promisePolyfill/index.js
@@ -4,6 +4,33 @@ class CustomPromise {
   rejectedData;
   rejectChain = [];
 
+  static all(promises) {
+    const fulfilled = [];
+    const result = [];
+
+    return new CustomPromise((_resolve, _reject) => {
+      promises.forEach((promise, index) => {
+        promise
+          .then((data) => {
+            fulfilled.push(true);
+
+            result[index] = data;
+
+            if (fulfilled.length === promises.length) _resolve(result);
+          })
+          .catch((error) => _reject(error));
+      });
+    });
+  }
+
+  static resolve(val) {
+    return new CustomPromise((_resolve) => _resolve(val));
+  }
+
+  static reject(error) {
+    return new CustomPromise((_resolve, _reject) => _reject(error));
+  }
+
   constructor(executor) {
     const resolve = (data) => {
       this.resolvedData = data;
