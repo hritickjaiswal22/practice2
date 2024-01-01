3f74d23d943167fd468ac7feb31c6b5da4fbcbba
Hritick Jaiswal <hritickjaiswal22@gmail.com>
2024-01-01 12:51:05 +0530

    debounce repractice

--- debounce/index.js
+++ debounce/index.js
@@ -1,16 +1,54 @@
-const btn = document.querySelector("button");
+// const btn = document.querySelector("button");
 
-function debounce(
-  fn,
-  delay = 200,
-  option = {
-    leading: false,
-    trailing: true,
-  }
-) {
+// function debounce(
+//   fn,
+//   delay = 200,
+//   option = {
+//     leading: false,
+//     trailing: true,
+//   }
+// ) {
+//   let timerId = null;
+//   const { leading, trailing } = option;
+//   let leadingFlag = true;
+
+//   return function () {
+//     clearTimeout(timerId);
+
+//     const args = arguments;
+//     const ctx = this;
+
+//     if (leading && leadingFlag) {
+//       fn.apply(ctx, args);
+//       leadingFlag = false;
+//     }
+
+//     timerId = setTimeout(() => {
+//       if (trailing) {
+//         fn.apply(ctx, args);
+//       }
+//       leadingFlag = true;
+//     }, delay);
+//   };
+// }
+
+// function print(args) {
+//   console.log("Printing");
+//   console.log(args);
+// }
+
+// const debouncedPrint = debounce(print, 1000, {
+//   leading: true,
+//   trailing: true,
+// });
+
+// btn.addEventListener("click", () => debouncedPrint("Hello"));
+
+// // IN LEADING OPTION IT WILL FIRE IMMEDIATELY
+
+// Normal debounce under 5 mins
+function debounce(fn, delay = 400) {
   let timerId = null;
-  const { leading, trailing } = option;
-  let leadingFlag = true;
 
   return function () {
     clearTimeout(timerId);
@@ -18,30 +56,36 @@ function debounce(
     const args = arguments;
     const ctx = this;
 
-    if (leading && leadingFlag) {
-      fn.apply(ctx, args);
-      leadingFlag = false;
-    }
-
-    timerId = setTimeout(() => {
-      if (trailing) {
-        fn.apply(ctx, args);
-      }
-      leadingFlag = true;
-    }, delay);
+    timerId = setTimeout(() => fn.apply(ctx, args), delay);
   };
 }
 
-function print(args) {
-  console.log("Printing");
-  console.log(args);
+// Leading debunce under 5 mins
+function leadingDebounce(fn, delay = 400) {
+  let timerId = null;
+  let flag = true;
+
+  return function () {
+    clearTimeout(timerId);
+
+    const args = arguments;
+    const ctx = this;
+
+    if (flag) {
+      fn.apply(ctx, args);
+      flag = false;
+    }
+
+    timerId = setTimeout(() => (flag = true), delay);
+  };
 }
 
-const debouncedPrint = debounce(print, 1000, {
-  leading: true,
-  trailing: true,
-});
+function expensive(name) {
+  console.log(`Hi ${name} this is expensive`);
+}
 
-btn.addEventListener("click", () => debouncedPrint("Hello"));
+const optimised = leadingDebounce(expensive);
 
-// IN LEADING OPTION IT WILL FIRE IMMEDIATELY
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => optimised("Hritick"));
