20c699208afe70cc3e1b39209ba389ccaaba9311
Hritick Jaiswal <hritickjaiswal22@gmail.com>
2024-01-06 10:07:10 +0530

    bfe elementAfer

---
+++ bfe/elementAfter.js
@@ -0,0 +1,19 @@
+function elementAfter(arr, target) {
+  let low = 0;
+  let high = arr.length - 1;
+  let lastIndex = Number.MIN_SAFE_INTEGER;
+
+  while (low <= high) {
+    const mid = low + Math.floor((high - low) / 2);
+
+    if (arr[mid] === target && mid > lastIndex) lastIndex = mid;
+
+    if (arr[mid] <= target) low = mid + 1;
+    else high = mid - 1;
+  }
+
+  if (lastIndex === Number.MIN_SAFE_INTEGER || lastIndex + 1 >= arr.length)
+    return undefined;
+
+  return arr[lastIndex + 1];
+}
