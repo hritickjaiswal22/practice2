87997b5a0918a57f11cb2afb312b671bcc793c97
Hritick Jaiswal <hritickjaiswal22@gmail.com>
2024-01-06 13:07:46 +0530

    bfe fib

--- bfe/fib.js
+++ bfe/fib.js
@@ -1,16 +1,32 @@
+// function fib(n) {
+//   if (n === 0) return 0;
+//   if (n === 1) return 1;
+//   if (n === 2) return 1;
+
+//   let prev1 = 1;
+//   let prev2 = 1;
+
+//   for (let i = 0; i < n - 2; i++) {
+//     const sum = prev1 + prev2;
+//     prev1 = prev2;
+//     prev2 = sum;
+//   }
+
+//   return prev2;
+// }
+
 function fib(n) {
-  if (n === 0) return 0;
-  if (n === 1) return 1;
-  if (n === 2) return 1;
+  function find(n) {
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    if (t[n] !== null) return t[n];
 
-  let prev1 = 1;
-  let prev2 = 1;
-
-  for (let i = 0; i < n - 2; i++) {
-    const sum = prev1 + prev2;
-    prev1 = prev2;
-    prev2 = sum;
+    return (t[n] = find(n - 1) + find(n - 2));
   }
 
-  return prev2;
+  const t = Array(n + 2).fill(null);
+
+  return find(n);
 }
+
+console.log(fib(10));
